e4f82fa67450292ff936c569e8215eb935a4ad45
Topi Talvitie <topi.talvitie@iki.fi>
2014-05-22 16:53:41 +0300

    Added functions for storing files by hash.

---
+++ file.cpp
@@ -0,0 +1,78 @@
+#include "file.hpp"
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+namespace cses {
+
+FileSave::FileSave() : saveCalled(false) {
+	tmpfile.exceptions(std::ofstream::eofbit | std::ofstream::failbit | std::ofstream::badbit);
+	if(!SHA1_Init(&shaCtx)) {
+		throw Error("FileSave::FileSave: SHA1_Init failed.");
+	}
+	
+	mkdir("files", 0700);
+	
+	char tmpfilenameArray[] = "files/tmp_XXXXXX";
+	int fd = mkstemp(tmpfilenameArray);
+	if(fd == -1) throw Error("FileSave::FileSave: Creating temporary file failed.");
+	close(fd);
+	
+	tmpfilename = tmpfilenameArray;
+	tmpfile.open(tmpfilename, std::ios_base::out | std::ios_base::binary);
+}
+
+FileSave::~FileSave() {
+	if(tmpfilename != "") {
+		unlink(tmpfilename.c_str());
+	}
+}
+
+void FileSave::write(const char* data, size_t length) {
+	if(!SHA1_Update(&shaCtx, data, length)) {
+		throw Error("FileSave::write: SHA1_Update failed");
+	}
+	tmpfile.write(data, length);
+}
+
+string FileSave::save() {
+	if(saveCalled) throw Error("FileSave::save: Called multiple times.");
+	saveCalled = true;
+	
+	tmpfile.close();
+	
+	uint8_t rawHash[SHA_DIGEST_LENGTH];
+	if(!SHA1_Final(rawHash, &shaCtx)) {
+		throw Error("FileSave::save: SHA1_Final failed");
+	}
+	
+	string hash;
+	const char* enc = "0123456789abcdef";
+	for(size_t i = 0; i < SHA_DIGEST_LENGTH; ++i) {
+		uint8_t byte = rawHash[i];
+		hash.push_back(enc[byte >> 4]);
+		hash.push_back(enc[byte & 15]);
+	}
+	
+	string filename = "files/" + hash;
+	if(rename(tmpfilename.c_str(), filename.c_str()) == -1) {
+		throw Error("FileSave::save: Could not move temporary file.");
+	} else {
+		tmpfilename = "";
+	}
+	
+	return hash;
+}
+
+unique_ptr<std::ifstream> openFileByHash(const string& hash) {
+	unique_ptr<std::ifstream> ret(new std::ifstream);
+	
+	string filename = "files/" + hash;
+	ret->open(filename);
+	
+	if(!ret->good()) throw Error("openFileByHash: Could not open file.");
+	
+	return ret;
+}
+
+}
---
+++ file.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#include "common.hpp"
+#include <fstream>
+#include <cstdlib>
+#include <openssl/sha.h>
+
+namespace cses {
+
+// Process for saving a file.
+class FileSave {
+public:
+	FileSave();
+	~FileSave();
+	
+	void write(const char* data, size_t length);
+	
+	// Save the file so that it can be opened with openFileByHash. Returns
+	// the hash. Should be called only once.
+	string save();
+	
+private:
+	void reset();
+	
+	bool saveCalled;
+	SHA_CTX shaCtx;
+	std::string tmpfilename;
+	std::ofstream tmpfile;
+};
+
+// Open file previously stored using FileSave by its hash. The stream is
+// returned as a pointer because of GCC bug, but it is never nullptr.
+// Parameter is not checked for sanity.
+unique_ptr<std::ifstream> openFileByHash(const string& hash);
+
+}
