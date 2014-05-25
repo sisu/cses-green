86b97b1057c77c2ea0042b5903b1e07ae8bf2953
Antti Laaksonen <ahslaaks@cs.helsinki.fi>
2014-05-25 04:06:38 +0300

    Debug prints removed 8)

--- common/file.cpp
+++ common/file.cpp
@@ -42,9 +42,7 @@ void FileSave::writeFileContents(const string& filename) {
 	
 	const size_t BUFSIZE = 4096;
 	
-	std::cerr << filename << std::endl;
 	while(!in.eof()) {
-		std::cerr << "lol" << std::endl;
 		char buf[BUFSIZE];
 		in.read(buf, BUFSIZE);
 		if(in.bad() || (in.fail() && !in.eof())) {
